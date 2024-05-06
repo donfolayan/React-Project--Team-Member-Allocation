f0aab6bf2c81c06bb06237f5637868e182e5274e
Folayan Donald <donfolayan@gmail.com>
2024-05-06 09:45:50 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import './App.css';
+import Header from './Header.jsx'
+import Content from './Content.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Content />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
